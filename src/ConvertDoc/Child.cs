4f7f892fd3c857e6443711ac3377978d4b281b30
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-02-04 12:30:31 +0100

    Model cleanup

--- src/ConvertDoc/Child.cs
+++ src/ConvertDoc/Child.cs
@@ -9,9 +9,14 @@ namespace ConvertDoc
         public Int32 Kind { get; set; }
         public String KindString { get; set; }
         public Flags Flags { get; set; }
-        public String OriginalName { get; set; }
+        public Comment Comment { get; set; }
+        public Signature[] Signatures { get; set; }
         public Source[] Sources { get; set; }
-        public Child1[] Children { get; set; }
+        public TypeClass Type { get; set; }
+        public String DefaultValue { get; set; }
+        public Typeparameter[] TypeParameter { get; set; }
+        public Child[] Children { get; set; }
         public Group[] Groups { get; set; }
+        public String OriginalName { get; set; }
     }
 }
